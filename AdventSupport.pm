a9b2e0f63c3899890bb4d10beed019c6ac669740
drbaggy <js5@sanger.ac.uk>
2020-12-03 06:35:22 +0000

    added chomp code and removed init call

--- AdventSupport.pm
+++ AdventSupport.pm
@@ -3,19 +3,28 @@ use strict;
 use warnings;
 use base qw(Exporter);
 
-our @EXPORT_OK   = qw(slurp_file init);
-our %EXPORT_TAGS = ('ALL'=>\@EXPORT_OK );
-my $ROOT_PATH;
+use File::Basename qw(dirname);
+use English qw(-no_match_vars $PROGRAM_NAME $PERL_VERSION);
+use Cwd qw(abs_path);
+use Data::Dumper;
+use Test::More;
+use Const::Fast;
 
-sub init {
-  $ROOT_PATH = $_[0];
-}
+our @EXPORT      = qw(slurp_file);
+our @EXPORT_OK   = @EXPORT;
+our %EXPORT_TAGS = ('ALL' => \@EXPORT );
+
+my $PROGRAM_PATH;
+BEGIN { $PROGRAM_PATH = dirname(abs_path($PROGRAM_NAME)); }
 
 sub slurp_file {
   my($fn,$file) = @_;
   $file ||= 'in.txt';
-  open my $fh, q(<), $ROOT_PATH.q(/).$file or die "Filename $file doesn't exist\n";
-  &{$fn}( $_ ) while <$fh>;
+  open my $fh, q(<), $PROGRAM_PATH.q(/).$file or die "Filename $file doesn't exist\n";
+  while(<$fh>) {
+    chomp;
+    &{$fn}( $_ );
+  }
   close $fh;
 }
 
