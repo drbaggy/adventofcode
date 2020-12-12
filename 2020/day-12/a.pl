047425fe0b33644d8c06bb08962ab0304ead6393
drbaggy <js5@sanger.ac.uk>
2020-12-12 07:58:28 +0000

    pushing all new files for day 12 solution

--- 2020/day-12/a.pl
+++ 2020/day-12/a.pl
@@ -18,18 +18,37 @@ use lib $ROOT_PATH;
 use AdventSupport;
 ## END OF BOILER PLATE;
 
-is( solution('test.txt'), 'result');
+my @D = ( [ 1, 0 ], [ 0, -1 ], [ -1, 0 ], [ 0, 1 ] ); # E,S,W,N
+
+is( solution('test.txt'), 25 );
 done_testing();
 
 say solution();
 
 sub solution {
   my $file_name = shift;
-  # Initialize slurp variables
+
+  my ($x,$y,$dir) = (0,0,0);
+
   slurp_file( sub {
+    my( $c, $n ) = $_[0] =~ m{([NSEWLRF])(\d+)};
+    return unless $c;
+    if( $c eq 'L' ) {
+      $dir -= $n / 90;
+    } elsif( $c eq 'R' ) {
+      $dir += $n / 90;
+    } elsif( $c eq 'F' ) {
+      $x += $n * $D[ $dir%4 ][ 0 ];
+      $y += $n * $D[ $dir%4 ][ 1 ];
+    } else {
+      $x += $n if $c eq 'E';
+      $x -= $n if $c eq 'W';
+      $y += $n if $c eq 'N';
+      $y -= $n if $c eq 'S';
+    }
     # Code to process each line of input
   }, $file_name );
-  ## Now the work horse bit
-  return 'result';
+
+  return abs($x) + abs($y);
 }
 
--- 2020/day-12/b.pl
+++ 2020/day-12/b.pl
@@ -18,7 +18,7 @@ use lib $ROOT_PATH;
 use AdventSupport;
 ## END OF BOILER PLATE;
 
-is( solution('test.txt'), 'result');
+is( solution('test.txt'), 286 );
 done_testing();
 
 say solution();
@@ -26,10 +26,28 @@ say solution();
 sub solution {
   my $file_name = shift;
   # Initialize slurp variables
+  my ($x,$y,$dir,$wx,$wy) = (0,0,0,10,1);
+
   slurp_file( sub {
-    # Code to process each line of input
+    my( $c,$n ) = $_[0] =~ m{([NSEWLRF])(\d+)};
+    return unless $c;
+    # Split into command and number
+    if( $c eq 'L' || $c eq 'R' ) { ## Rotate way point
+      $n = 360 - $n if $c eq 'L';
+      ( $wx, $wy ) = ( -$wx, -$wy ) if $n == 180;
+      ( $wx, $wy ) = ( $wy,  -$wx ) if $n ==  90;
+      ( $wx, $wy ) = ( -$wy,  $wx ) if $n == 270;
+    } elsif( $c eq 'F' ) { ## Move ship
+      $x += $n * $wx;
+      $y += $n * $wy;
+    } else { ## Move waypoint
+      $wx += $n if $c eq 'E';
+      $wx -= $n if $c eq 'W';
+      $wy += $n if $c eq 'N';
+      $wy -= $n if $c eq 'S';
+    }
   }, $file_name );
-  ## Now the work horse bit
-  return 'result';
+
+  return abs($x) + abs($y);
 }
 
