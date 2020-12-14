a18f60c4aa578e3b66847a208e2448b872bceb37
James Smith <js5@sanger.ac.uk>
2020-12-14 00:47:00 +0000

    Formatting

--- 2020/day-13/a.pl
+++ 2020/day-13/a.pl
@@ -18,14 +18,19 @@ use lib $ROOT_PATH;
 use AdventSupport;
 ## END OF BOILER PLATE;
 
-is( solution(939,'7,13,x,x,59,x,31,19'),295);
+is( solution( 939, '7,13,x,x,59,x,31,19' ), 295 );
 done_testing();
 
-say solution(1005595,'41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,557,x,29,x,x,x,x,x,x,x,x,x,x,13,x,x,x,17,x,x,x,x,x,23,x,x,x,x,x,x,x,419,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19');
+say solution(
+ '41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,'.
+ 'x,x,x,x,x,37,x,x,x,x,x,557,x,29,x,x,x,x,x,x,x,x,x,x,13,x,x,x,'.
+ '17,x,x,x,x,x,23,x,x,x,x,x,x,x,419,x,x,x,x,x,x,x,x,x,x,x,x,x,x'.
+ ',x,x,x,x,19'
+);
 
 sub solution {
   my( $id, $next, $t0, @times ) = ( undef, 1e30, $_[0], grep { m{\d} } split m{,}, $_[1] );
-  ($id,$next) = $_ - ( $t0 % $_ ) > $next ? ($id,$next) : ($_,$_ - ( $t0 % $_ )) for @times;
-  return $next * $id ;
+  ($id,$next) = $_ - ( $t0 % $_ ) > $next ? ($id,$next) : ($_,$_ - ( $t0 % $_ )) foreach @times;
+  return $next * $id;
 }
 
--- 2020/day-13/b.pl
+++ 2020/day-13/b.pl
@@ -18,16 +18,21 @@ use lib $ROOT_PATH;
 use AdventSupport;
 ## END OF BOILER PLATE;
 
-is( solution('7,13,x,x,59,x,31,19'),1068781);
-is( solution('17,x,13,19'),        3417);
-is( solution('67,7,59,61'),        754018);
-is( solution('67,x,7,59,61'),      779210);
-is( solution('67,7,x,59,61'),      1261476);
-is( solution('1789,37,47,1889'),   1202161486);
+is( solution( '7,13,x,x,59,x,31,19' ),    1068781 );
+is( solution( '17,x,13,19'          ),       3417 );
+is( solution( '67,7,59,61'          ),     754018 );
+is( solution( '67,x,7,59,61'        ),     779210 );
+is( solution( '67,7,x,59,61'        ),    1261476 );
+is( solution( '1789,37,47,1889'     ), 1202161486 );
 
 done_testing();
 
-say solution('41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,557,x,29,x,x,x,x,x,x,x,x,x,x,13,x,x,x,17,x,x,x,x,x,23,x,x,x,x,x,x,x,419,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,19');
+say solution(
+ '41,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,'.
+ 'x,x,x,x,x,37,x,x,x,x,x,557,x,29,x,x,x,x,x,x,x,x,x,x,13,x,x,x,'.
+ '17,x,x,x,x,x,23,x,x,x,x,x,x,x,419,x,x,x,x,x,x,x,x,x,x,x,x,x,x'.
+ ',x,x,x,x,19'
+);
 
 sub solution {
   my ( $result, $index, $mult, @times ) = ( 0, 0, split m{,}, $_[0] );
@@ -35,8 +40,16 @@ sub solution {
     $index++;
     next if $_ eq 'x';
     $result += $mult while ($result+$index) % $_;
-    $mult *= $_;
+    $mult = lcm($mult,$_);
   }
   return $result;
+}
+
+sub lcm {
+  my ($a,$b) = @_;
+  ($a,$b) = ($b,$a) if $b>$a;
+  my $p = $a * $b;
+  ($a,$b) = ($b,$a%$b) while $b >= 1;
+  return $p/$a;
 }
 
