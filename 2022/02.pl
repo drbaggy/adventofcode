7e0f32a21bd3817651b303f45a4f632e7b9646e3
James Smith <js5@sanger.ac.uk>
2022-12-02 06:30:07 +0000

    Update 02.pl

--- 2022/02.pl
+++ 2022/02.pl
@@ -1,8 +1,6 @@
 use strict;
 use warnings;
 use feature qw(say);
-use Data::Dumper qw(Dumper);
-use Carp qw(croak);
 
 ## We could do some splitting and nice function to work out
 ## the score for each line, but we don't need to - we can
@@ -18,7 +16,7 @@ my($n,%sn)=(0,'A X'=>3,'A Y'=>4,'A Z'=>8,'B X'=>1,'B Y'=>5,'B Z'=>9,'C X'=>2,'C 
 ## Loop through the file, keeping a running total for both
 ## part 1 & part 2, (in $t & $n respectively)
 
-open my $fh, '<', 'data/02.txt' or croak 'No data';
+open my $fh, '<', 'data/02.txt';
 chomp, $t+=$st{$_}, $n+=$sn{$_} while <$fh>;
 close $fh;
 
