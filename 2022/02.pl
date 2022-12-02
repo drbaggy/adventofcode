a46315374ee38d2fa971c27c3f0037f9579a48db
James Smith <js5@sanger.ac.uk>
2022-12-02 06:24:09 +0000

    Update 02.pl

--- 2022/02.pl
+++ 2022/02.pl
@@ -4,17 +4,25 @@ use feature qw(say);
 use Data::Dumper qw(Dumper);
 use Carp qw(croak);
 
-# We could do some splitting and nice function
-# But we know the score for each line.
-# %st is the scores for part 1 (and we use $t as the total)
-# %sn is the scores for part 2 (and we use $n as the total)
+## We could do some splitting and nice function to work out
+## the score for each line, but we don't need to - we can
+## simply use a look up as we only have 9 combinations and
+## each combination gives one score.
+
+## %st is the scores for part 1 (and we use $t as the total)
+## %sn is the scores for part 2 (and we use $n as the total)
 
 my($t,%st)=(0,'A X'=>4,'A Y'=>8,'A Z'=>3,'B X'=>1,'B Y'=>5,'B Z'=>9,'C X'=>7,'C Y'=>2,'C Z'=>6);
 my($n,%sn)=(0,'A X'=>3,'A Y'=>4,'A Z'=>8,'B X'=>1,'B Y'=>5,'B Z'=>9,'C X'=>2,'C Y'=>6,'C Z'=>7);
 
+## Loop through the file, keeping a running total for both
+## part 1 & part 2, (in $t & $n respectively)
+
 open my $fh, '<', 'data/02.txt' or croak 'No data';
-chomp,$t+=$st{$_},$n+=$sn{$_} while <$fh>;
+chomp, $t+=$st{$_}, $n+=$sn{$_} while <$fh>;
 close $fh;
+
+## Output the two scores:
 
 say"$t\n$n"
 
