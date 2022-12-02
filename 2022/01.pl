1599971fcf42dd683a8194e74b7fb24cee2eb747
James Smith <js5@sanger.ac.uk>
2022-12-02 05:58:54 +0000

    Update 01.pl

--- 2022/01.pl
+++ 2022/01.pl
@@ -1,12 +1,24 @@
 use strict;
 use warnings;
 use feature qw(say);
-use Data::Dumper qw(Dumper);
-use Carp qw(croak);
 
-open my $fh, '<', 'data/01.txt' or croak 'No data';
-my @elves=(0);
-/\d/mxs?($elves[-1]+=$_):push@elves,0 while<$fh>;
+## Elves is the list of calories carried by each
+## elf.
+
+my @e=(0);
+
+## We loop through the file - if the line is blank
+## we add another elf to the list and initialize
+## the calories as 0 o/w we add the calories to the
+## last elf in the list.
+
+open my $fh, '<', 'data/01.txt';
+/\d/ ? ($e[-1]+=$_) : push@e,0 while<$fh>;
 close $fh;
 
-say for sub { $_[-1], $_[-1]+$_[-2]+$_[-3] }->(sort {$a<=>$b} @elves);
+## We use a self executing closure to take the
+## output of a numeric sort to compute the highest
+## total for an elf AND the highest total for the
+## top three elves.
+
+say for sub { $_[0], $_[0]+$_[1]+$_[2] }->(sort {$b<=>$a} @e);
