b43664e41acfacac0836b3812c704797179ff5a3
James Smith <drbaggy@gmail.com>
2022-12-02 05:41:56 +0000

    day 2

---
+++ 2022/02.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use feature qw(say);
+use Data::Dumper qw(Dumper);
+use Carp qw(croak);
+
+my($t,%st)=(0,'A X'=>4,'A Y'=>8,'A Z'=>3,'B X'=>1,'B Y'=>5,'B Z'=>9,'C X'=>7,'C Y'=>2,'C Z'=>6);
+my($n,%sn)=(0,'A X'=>3,'A Y'=>4,'A Z'=>8,'B X'=>1,'B Y'=>5,'B Z'=>9,'C X'=>2,'C Y'=>6,'C Z'=>7);
+open my $fh, '<', 'data/02.txt' or croak 'No data';
+chomp,$t+=$st{$_},$n+=$sn{$_} while <$fh>;
+close $fh;
+say"$t\n$n"
+
