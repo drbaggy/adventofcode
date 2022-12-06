cb7e524f3f152630345d35a33928504d54168ece
James Smith <drbaggy@gmail.com>
2022-12-06 12:07:27 +0000

    added coding comments

--- 2022/06.pl
+++ 2022/06.pl
@@ -3,11 +3,30 @@ use feature qw(say);
 use Time::HiRes qw(time);
 my $time = time;
 
+## We slurp the string in from the file.
+
 open my $fh, q(<), 'data/06.txt' or die "No input";
 my $input = <$fh>;
 close $fh;
+
+## We now look for solutions for markers of length 4 and 14.
+
 say find_marker($input, $_) for 4,14;
-## Do stuff here...
+
+## find_marker takes two inputs the string, and the size of
+## markers
+
+## We split the string into, the first "n" characters
+## ($block) and the rest of the string ($str). Finally
+## we set up $n to represent the number of characters
+## processed - at the moment $n.
+
+## We then loop through the remaining string.
+## The block contains a duplicate letter, we incrememnt
+## the position ($n) by one, and remove the first letter
+## from $block, and the first letter from $str to the
+## end. If there isn't a match we have found the mark
+## and return
 
 sub find_marker {
   my $block = substr my $str = $_[0], 0, my $n = $_[1], '';
