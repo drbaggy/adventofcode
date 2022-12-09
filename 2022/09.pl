5b2b0ba3f99c52ffd66f0fcb83509dd4691543bc
James Smith <drbaggy@gmail.com>
2022-12-09 07:28:24 +0000

    09 sol

--- 2022/09.pl
+++ 2022/09.pl
@@ -1,12 +1,39 @@
 use strict;
 use warnings;
 use feature qw(say);
+
+use Data::Dumper qw(Dumper);
 use Time::HiRes qw(time);
+
 my $time = time;
 
-my $t = my $n = 0;
+my(%v,@res) = ( 'U' => [0,1], 'D' => [0,-1],
+                'R' => [1,0], 'L' => [-1,0] );
 
-## Do stuff here...
+open my $fh, '<', 'data/09.txt';
+my @lines = <$fh>;
+close $fh;
 
-say "\nTime :", sprintf '%0.6f', time-$time;
-say"$t\n$n";
+for my $K ( 1, 9 ) {  ## 2 & 10 knots
+  my(@p,%seen) = map { [0,0] } 0.. $K;
+  for( @lines ) {
+    my($d,$l)=split;
+    for (1..$l) {
+      ( $p[0][0] += $v{$d}[0], $p[0][1] += $v{$d}[1] );
+      (    $p[$_][0] - $p[$_-1][0] < 2
+        && $p[$_-1][0] - $p[$_][0] < 2
+        && $p[$_][1] - $p[$_-1][1] < 2
+        && $p[$_-1][1] - $p[$_][1] < 2
+      ) || (
+        $p[$_-1][0]>$p[$_][0] && $p[$_][0]++,
+        $p[$_-1][1]>$p[$_][1] && $p[$_][1]++,
+        $p[$_-1][0]<$p[$_][0] && $p[$_][0]--,
+        $p[$_-1][1]<$p[$_][1] && $p[$_][1]--
+      ) for 1..$K;
+      $seen{ $p[$K][0].'/'.$p[$K][1] }++;
+    }
+  }
+  push @res, scalar values %seen;
+}
+say "Time :", sprintf '%0.6f', time-$time;
+say for @res;
